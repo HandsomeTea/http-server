20252d459f68fd429915a65fb25cac88658d52d2
haifeng_liu <haifeng_liu@bizconf.cn>
2019-11-02 11:47:55 +0800

    code update

--- middlewares/authorization/index.js
+++ middlewares/authorization/index.js
@@ -2,10 +2,21 @@ import _validate from './json-web-token';
 
 const _JWTcheck = new _validate(process.env.JWT_APP_NAME, process.env.JWT_APP_ID, process.env.JWT_APP_SECERT);
 
+/**
+ * 生成json web token
+ * @returns
+ */
 export const JWTgeneral = () => {
     return _JWTcheck.sign();
 };
 
+/**
+ * 验证json web token
+ *
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
 export const JWTcheck = (req, res, next) => {
     if (req.headers.authorization) {
         const _array = req.headers.authorization.split(' ');
--- utils/http-result/response.js
+++ utils/http-result/response.js
@@ -13,62 +13,93 @@ export default (_response) => new class resReult {
         }
         this.response = _response;
         this.request = _response.req;
+        this._init();
+    }
+    /**
+     * 初始化
+     */
+    _init() {
+
+    }
+    /**
+     * 所有http请求的回返结构定义
+     * @param {*} _data
+     * @param {*} _httpStatusCode
+     * @param {*} _httpStatusType
+     */
+    _resultDefine(_data, _httpStatusCode, _httpStatusType) {
+        this.response.status(_httpStatusCode);
+        const _format = {
+            result: _httpStatusCode === 200,
+            type: _httpStatusType
+        };
+
+        if (_httpStatusCode === 200) {
+            _format.data = _data;
+        } else {
+            _format.info = _data;
+        }
+        this._resSend(_format);
     }
 
+    /**
+     * request处理成功的封装
+     * @param {*} _data
+     * @param {*} _type
+     */
     success(_data, _type) {
-        this.response.status(200);
-        this._resSend({
-            result: true,
-            type: _type || httpStatus.success,
-            data: _data
-        });
+        this._resultDefine(_data, 200, _type || httpStatus.success);
     }
 
+    /**
+     * request处理失败的封装
+     * @param {*} _data
+     * @param {*} _type
+     */
     failure(_data, _type) {
-        this.response.status(400);
-        this._resSend({
-            result: false,
-            type: _type || httpStatus.failed,
-            info: _data
-        });
+        this._resultDefine(_data, 400, _type || httpStatus.failed);
     }
 
+    /**
+     * request处理对象未找到的封装
+     * @param {*} _data
+     * @param {*} _type
+     */
     notFound(_data, _type) {
-        this.response.status(404);
-        this._resSend({
-            result: false,
-            type: _type || httpStatus.notFound,
-            info: _data
-        });
+        this._resultDefine(_data, 404, _type || httpStatus.notFound);
     }
 
-    internalError(_data, _type) {
-        this.response.status(500);
-        this._resSend({
-            result: false,
-            type: _type || httpStatus.innerError,
-            info: _data
-        });
-    }
-
+    /**
+     * request处理未授权的封装
+     * @param {*} _data
+     * @param {*} _type
+     */
     unauthorized(_data, _type) {
-        this.response.status(403);
-        this._resSend({
-            result: false,
-            type: _type || httpStatus.noPermission,
-            info: _data
-        });
+        this._resultDefine(_data, 403, _type || httpStatus.noPermission);
     }
 
+    /**
+     * request太多太频繁的封装
+     * @param {*} _data
+     * @param {*} _type
+     */
     tooManyRequests(_data, _type) {
-        this.response.status(429);
-        this._resSend({
-            result: false,
-            type: _type || httpStatus.tooMany,
-            info: _data
-        });
+        this._resultDefine(_data, 429, _type || httpStatus.tooMany);
     }
 
+    /**
+     * 服务器处理request出错的封装
+     * @param {*} _data
+     * @param {*} _type
+     */
+    internalError(_data, _type) {
+        this._resultDefine(_data, 500, _type || httpStatus.innerError);
+    }
+
+    /**
+     * http请求统一回返
+     * @param {*} _result
+     */
     _resSend(_result) {
         const addressIpv4 = (this.request.headers['x-forwarded-for'] || this.request.connection.remoteAddress || this.request.socket.remoteAddress || this.request.connection.socket.remoteAddress).match(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/);
 
