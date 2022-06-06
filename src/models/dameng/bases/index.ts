4cc9205169f9f3f7eba2483bbc61ff2f0dd54de8
haifeng_liu@bizconf.cn <haifeng_liu@bizconf.cn>
2022-06-06 14:52:35 +0800

    code update

--- src/models/dameng/bases/index.ts
+++ src/models/dameng/bases/index.ts
@@ -1,3 +1,10 @@
 import DMORM from './orm';
 
 export { DMORM };
+export { DmModel } from './typings';
+
+export const DmType: { DATE: 'DATE', NUMBER: 'NUMBER', STRING: 'STRING' } = {
+    DATE: 'DATE',
+    NUMBER: 'NUMBER',
+    STRING: 'STRING'
+};
--- src/models/dameng/user.ts
+++ src/models/dameng/user.ts
@@ -1,11 +1,13 @@
-import { DMORM } from './bases';
+import { DmModel, DMORM, DmType } from './bases';
 
 export default class User extends DMORM<UserModel> {
     constructor() {
-        super('user', {
-            _id: { type: 'STRING' },
-            name: { type: 'NUMBER' },
-            test: { type: 'DATE' }
-        });
+        const model: DmModel<UserModel> = {
+            _id: { type: DmType.STRING },
+            name: { type: DmType.NUMBER },
+            test: { type: DmType.DATE }
+        };
+
+        super('user', model);
     }
 }
